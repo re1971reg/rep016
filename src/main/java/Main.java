755d9428494680b1d41258d6d0ae77bce02f5b4a
re1971reg <re1971reg@gmail.com>
2022-02-14 09:25:41 +0300

    корректировка /Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,18 +13,17 @@ public class Main {
     public static final int MAX_SIZE = 11;
 
     public static void main(String[] args) {
-
-        IndexStorage indexStorage = new IndexStorage(MAX_SIZE);
-        System.out.printf("indexStorage.length=[%d]\n", indexStorage.size());
-        for (int i = 0; i < indexStorage.size(); i++) {
-            System.out.printf("indexStorage[%d]=%d\n", i, indexStorage.get(i));
-        }
-        System.out.println("reverse()=" + Arrays.toString(indexStorage.reverse()));
-
-        int[] arr = {0, -10, 20, -30, 40, -50, 60, -70, -80, -90, 100, -110, 120, -130, 140};
-
-        IndexStorage arrayIndexStorage = new ArrayIndexStorage(arr);
-        System.out.printf("arrayIndexStorage.length=[%d]\n", arrayIndexStorage.size());
-        System.out.println("reverse()=" + Arrays.toString(arrayIndexStorage.reverse()));
+//        IndexStorage indexStorage = new IndexStorage(MAX_SIZE);
+//        System.out.printf("indexStorage.length=[%d]\n", indexStorage.size());
+//        for (int i = 0; i < indexStorage.size(); i++) {
+//            System.out.printf("indexStorage[%d]=%d\n", i, indexStorage.get(i));
+//        }
+//        System.out.println("reverse()=" + Arrays.toString(indexStorage.reverse()));
+//
+//        int[] arr = {0, -10, 20, -30, 40, -50, 60, -70, -80, -90, 100, -110, 120, -130, 140};
+//
+//        IndexStorage arrayIndexStorage = new ArrayIndexStorage(arr);
+//        System.out.printf("arrayIndexStorage.length=[%d]\n", arrayIndexStorage.size());
+//        System.out.println("reverse()=" + Arrays.toString(arrayIndexStorage.reverse()));
     }
 }
